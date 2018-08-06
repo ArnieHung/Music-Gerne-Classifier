b6345db41a14a118ab513b6dc6793ceb84c26b1d
arnie <arniehung@gmail.com>
2018-08-06 15:54:10 +0800

    ''

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,18 @@
+
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+let audioCtx;
+
+const Record = document.getElementById("Record");
+const Stop = document.getElementById("Stop");
+
+Record.onclick = () => {
+    audioCtx = new AudioContext();
+    setupNodes();
+}
+
+Stop.onclick = () => {
+    audioCtx.close();
+}
 
 let stream;
 
@@ -9,9 +22,42 @@ let processor;
 let bufferLength;
 
 const FREQ_NUM = 128;
-const PROCESS_NUM = 1204;
+const PROCESS_NUM = 1024;
+
+const canvas = document.getElementById("visualizer");
+let ctx = canvas.getContext("2d");
+
+const canvasTmp = document.createElement("canvas");
+canvasTmp.width=300;
+canvasTmp.height=200;
+let ctxTmp = canvasTmp.getContext("2d");
 
 
+
+let samplesCnt = 0;
+let dataArray =  new Uint8Array(FREQ_NUM * FREQ_NUM);
+
+
+function predict (dataArray) {
+    console.log(dataArray);
+}
+
+function draw(dx) {
+
+    ctxTmp.drawImage(canvas, 0, 0, 300, 200,  0, 0, 300, 200);
+
+    for (let i = 0; i < FREQ_NUM; i++) {
+        var value = dx[i];
+        dataArray[samplesCnt*FREQ_NUM + i] = value;
+
+        ctx.fillStyle = `rgb( ${value}, ${value}, ${value})`;
+        ctx.fillRect(300 - 1, 200 - i, 1, 1);
+    }
+    ctx.translate(-1, 0);
+    ctx.drawImage( canvasTmp, 0, 0, 300, 200, 0, 0, 300, 200);
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+}
 
 
 function setupNodes() {
@@ -27,13 +73,18 @@ function setupNodes() {
                 analyser.fftSize = FREQ_NUM * 2;
                 bufferLength = analyser.frequencyBinCount;
 
-            
-
                 processor = audioCtx.createScriptProcessor(PROCESS_NUM, 1, 1);
                 processor.connect(audioCtx.destination);
                 processor.onaudioprocess = () => {
-                    
-                   
+                    let dx = new Uint8Array(analyser.frequencyBinCount);
+                    analyser.getByteFrequencyData(dx);
+
+                    draw(dx); 
+                  
+                    if((++samplesCnt) == 128 ) {
+                        predict(dataArray);
+                        samplesCnt = 0;
+                    }      
                 }
 
                 source.connect(analyser);
@@ -50,5 +101,4 @@ function setupNodes() {
     
 // } 
 
- setupNodes();
  //connectNodes();
